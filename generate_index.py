676f705d6b80c0c88233b67e6da1f55a528e0d83
Sherwin Chen <chencw@umich.edu>
2021-05-31 15:20:33 +0800

    start testing

---
+++ generate_index.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import json
+
+
+DIRPATHS = 0
+DIRNAMES = 1
+FILENAMES = 2
+
+print(next(os.walk('.'))[DIRPATHS])
+print(next(os.walk('.'))[DIRNAMES])
+print(next(os.walk('.'))[FILENAMES])
+
+result = { }
+
+song_directories = next(os.walk('.'))[DIRNAMES]
+
+for song_direcory in song_directories:
+    if song_direcory.startswith("."):
+        continue
+
+    files = next(os.walk(song_direcory))[FILENAMES]
+
+    for file in files:
+
+        if file.startswith(".") or file.endswith(".meta"):
+            continue
+
+        file_path = f"{song_direcory}/{file}"
+        
+        result[file_path] = { }
+
+        # check modified time
+        # check path
+        # audio
+        result[file_path]["modified date"] = os.path.getmtime(file_path)
+        result[file_path]["size"] = os.path.getsize(file_path)
+
+y = json.dumps(result, indent=4)
+
+with open("index.json", "w") as index:
+    index.write(y)
