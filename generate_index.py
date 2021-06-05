6ad574ae9eda12499d56205c04d3bb06cd586154
Sherwin Chen <chencw@umich.edu>
2021-06-06 03:24:57 +0800

    Update generate_index.py

--- generate_index.py
+++ generate_index.py
@@ -33,9 +33,6 @@ for song_direcory in song_directories:
         modification_sec = os.path.getmtime(file_path)
         m_date = str(datetime.fromtimestamp(modification_sec))
 
-        # check modified time
-        # check path
-        # audio
         result[file_path]["ModificationDate"] = m_date
         result[file_path]["Size"] = os.path.getsize(file_path)
 
